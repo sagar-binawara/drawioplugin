d1cb06b4302d655a4101f212fdcec1c95e4aa1ca
sagar-binawara <56292306+sagar-binawara@users.noreply.github.com>
2019-10-08 18:24:18 +0530

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -2,8 +2,8 @@
  * Sample plugin.
  */
 Draw.loadPlugin(function(ui) {
-	console.log(ui);
 	console.log(ui.sidebar);
+	console.log(ui.sidebar.palettes);
     // Adds custom sidebar entry
  ui.sidebar.addPalette('esolia', 'eSolia', true, function(content) {
 
